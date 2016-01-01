1f1c8d23765e1e5fbd7820bbe85c31ed3b84425c
tyler <tgl@rams.colostate.edu>
2016-01-01 11:19:02 -0700

    Changes to modes example.

--- examples/BME280_Modes/BME280_Modes.ino
+++ examples/BME280_Modes/BME280_Modes.ino
@@ -30,8 +30,40 @@ Last Updated: Jan 1 2016.
 /* ==== END Defines ==== */
 
 /* ==== Global Variables ==== */
-BME280 bme;                   // Default : forced mode, standby time = 1000 ms
-                              // pressure ×1, temperature ×1, humidity ×1, filter off,
+BME280 bme;                     // Default : forced mode, standby time = 1000 ms
+                                // pressure ×1, temperature ×1, humidity ×1, filter off
+
+/* Based on Bosch BME280 environmental sensor data sheet. */
+
+//BME280 bme;                   // Weather Monitoring : forced mode, 1 sample/minute
+                                // pressure ×1, temperature ×1, humidity ×1, filter off
+                                // Current Consumption =  0.16 μA
+                                // RMS Noise = 3.3 Pa/30 cm, 0.07 %RH
+                                // Data Output Rate 1/60 Hz
+
+//BME280 bme(0, 1, 1);          // Humidity Sensing : forced mode, 1 sample/second
+                                // pressure ×0, temperature ×1, humidity ×1, filter off
+                                // Current Consumption = 2.9 μA
+                                // RMS Noise = 0.07 %RH
+                                // Data Output Rate =  1 Hz
+
+//BME280 bme(5, 2, 1, 1, 0, 4); // Indoor Navigation : normal mode, standby time = 0.5ms
+                                // pressure ×16, temperature ×2, humidity ×1, filter = x16
+                                // Current Consumption = 633 μA
+                                // RMS Noise = 0.2 Pa/1.7 cm
+                                // Data Output Rate = 25Hz
+                                // Filter Bandwidth = 0.53 Hz
+                                // Response Time (75%) = 0.9 s
+
+
+//BME280 bme(3, 1, 0, 1, 0, 4); // Gaming : normal mode, standby time = 0.5ms
+                                // pressure ×4, temperature ×1, humidity ×0, filter = x16
+                                // Current Consumption = 581 μA
+                                // RMS Noise = 0.3 Pa/2.5 cm
+                                // Data Output Rate = 83 Hz
+                                // Filter Bandwidth = 1.75 Hz
+                                // Response Time (75%) = 0.3 s
+
 bool metric = false;
 /* ==== END Global Variables ==== */
 
