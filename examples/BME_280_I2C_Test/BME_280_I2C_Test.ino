b512ae217d69b38f8dd43e37df5edeb5ef66a05d
Tyler Glenn <finitespace@github.com>
2017-09-20 00:05:56 -0600

    Updating BME_280_I2C_Test.ino.

--- examples/BME_280_I2C_Test/BME_280_I2C_Test.ino
+++ examples/BME_280_I2C_Test/BME_280_I2C_Test.ino
@@ -3,23 +3,12 @@ BME280 I2C Test.ino
 This code shows how to record data from the BME280 environmental sensor
 using I2C interface. This file is an example file, part of the Arduino
 BME280 library.
-Copyright (C) 2016  Tyler Glenn
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+GNU General Public License
 
 Written: Dec 30 2015.
-Last Updated: Sep 19 2016.
+Last Updated: Sep 19 2017.
 
 Connecting the BME280 Sensor:
 Sensor              ->  Board
@@ -31,62 +20,51 @@ SCK (Serial Clock)  ->  A5 on Uno/Pro-Mini, 21 on Mega2560/Due, 3 Leonardo/Pro-M
 
  */
 
-/* ==== Includes ==== */
 #include <BME280I2C.h>
 #include <Wire.h>             // Needed for legacy versions of Arduino.
-/* ====  END Includes ==== */
 
-/* ==== Defines ==== */
 #define SERIAL_BAUD 115200
-/* ==== END Defines ==== */
 
-/* ==== Global Variables ==== */
 BME280I2C bme;                   // Default : forced mode, standby time = 1000 ms
                               // Oversampling = pressure ×1, temperature ×1, humidity ×1, filter off,
 bool metric = false;
-/* ==== END Global Variables ==== */
 
-
-/* ==== Prototypes ==== */
-/* === Print a message to stream with the temp, humidity and pressure. === */
-void printBME280Data(Stream * client);
-/* === Print a message to stream with the altitude, and dew point. === */
-void printBME280CalculatedData(Stream* client);
-/* ==== END Prototypes ==== */
-
-/* ==== Setup ==== */
-void setup() {
+//////////////////////////////////////////////////////////////////
+void setup()
+{
   Serial.begin(SERIAL_BAUD);
+
   while(!Serial) {} // Wait
-  while(!bme.begin()){
+
+  Wire.begin();
+  
+  while(!bme.begin())
+  {
     Serial.println("Could not find BME280 sensor!");
     delay(1000);
   }
 }
-/* ==== END Setup ==== */
 
-/* ==== Loop ==== */
-void loop() {
+//////////////////////////////////////////////////////////////////
+void loop()
+{
    printBME280Data(&Serial);
    printBME280CalculatedData(&Serial);
    delay(500);
 }
-/* ==== End Loop ==== */
 
-/* ==== Functions ==== */
-void printBME280Data(Stream* client){
+//////////////////////////////////////////////////////////////////
+void printBME280Data
+(
+   Stream* client
+)
+{
   float temp(NAN), hum(NAN), pres(NAN);
-   uint8_t pressureUnit(3);                                           // unit: B000 = Pa, B001 = hPa, B010 = Hg, B011 = atm, B100 = bar, B101 = torr, B110 = N/m^2, B111 = psi
-   bme.read(pres, temp, hum, metric, pressureUnit);                   // Parameters: (float& pressure, float& temp, float& humidity, bool celsius = false, uint8_t pressureUnit = 0x0)
-  /* Alternatives to ReadData():
-    float temp(bool celsius = false);
-    float pres(uint8_t unit = 0);
-    float hum();
 
-    Keep in mind the temperature is used for humidity and
-    pressure calculations. So it is more effcient to read
-    temperature, humidity and pressure all together.
-   */
+   uint8_t pressureUnit(3);                                           // unit: B000 = Pa, B001 = hPa, B010 = Hg, B011 = atm, B100 = bar, B101 = torr, B110 = N/m^2, B111 = psi
+   
+   bme.read(pres, temp, hum, metric, pressureUnit);                   // Parameters: (float& pressure, float& temp, float& humidity, bool celsius = false, uint8_t pressureUnit = 0x0)
+
   client->print("Temp: ");
   client->print(temp);
   client->print("°"+ String(metric ? 'C' :'F'));
@@ -97,7 +75,10 @@ void printBME280Data(Stream* client){
   client->print(pres);
   client->print(" atm");
 }
-void printBME280CalculatedData(Stream* client){
+
+//////////////////////////////////////////////////////////////////
+void printBME280CalculatedData(Stream* client)
+{
   float altitude = bme.alt(metric);
   float dewPoint = bme.dew(metric);
   client->print("\t\tAltitude: ");
@@ -108,4 +89,3 @@ void printBME280CalculatedData(Stream* client){
   client->println("°"+ String(metric ? 'C' :'F'));
 
 }
-/* ==== END Functions ==== */
