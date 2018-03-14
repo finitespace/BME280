9b85afeed24400973d075f04f0f7f26ea2fe7428
finitespace <finitespaceghb2@junk.yoglenn.com>
2018-03-13 21:01:31 -0600

    Changing config.h to Config.h

---
+++ src/Config.h
@@ -0,0 +1,35 @@
+/*
+
+config.h
+
+This sets the configuration for the Arduino BME280 library.
+Copyright (C) 2016  Tyler Glenn
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Written: Mar 13 2018.
+
+This code is licensed under the GNU LGPL and is open for distrbution
+and copying in accordance with the license.
+This header must be included in any derived code or copies of the code.
+
+ */
+
+#ifndef TG_BME_280_CONFIG_H
+#define TG_BME_280_CONFIG_H
+
+#define USING_SOFTWARE_WIRE 1
+//#define USING_BRZO 1
+
+#endif
